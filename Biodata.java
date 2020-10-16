60e29a04fa3d8470fbded2b9e0aefe62668dd0c6
Ani <70193865+AniAtunnisak@users.noreply.github.com>
2020-10-16 14:23:09 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ANI
+ */
+public class Biodata {
+     public void namaLengkap(){
+       System.out.println("Nama Lengkap : Ani Atunisak");
+    }
+     
+    public void kelas(){
+       System.out.println(" Kelas : XI");
+    }
+    public void jurusan(){
+       System.out.println(" Jurusan : Rekayasa Perangkat Lunak");
+    }
+    public void noAbsen(){
+       System.out.println(" No.Absen : 03(tiga)");
+    }
+    public void tempatLahir(){
+       System.out.println(" TempatLahir : Pasuruan");
+    }
+    public void tanggalLahir(){
+       System.out.println(" TanggalLahir : 01 Desember 2003 ");
+    }
+    public void hobi(){
+       System.out.println(" Hobi : Berenang dan Memasak ");
+    }
+    public void alamatLengkap(){
+       System.out.println(" Alamat Lengkap: Dsn.Gunung Sari, Ds.Kertosari, Kec.Purwosari, Kab.Pasuruan, Provinsi.Jawa Timur, wilayah.Indonesia");
+    }
+  } 
---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ANI
+ */
+public class Main {
+    public static void main(String [] args) {
+        Biodata biodata = new Biodata();
+        biodata.namaLengkap();
+        biodata.kelas();
+        biodata.jurusan();
+        biodata.noAbsen();
+        biodata.tempatLahir();
+        biodata.tanggalLahir();
+         biodata.hobi();
+        biodata.alamatLengkap();
+     }
+}
